2240911801a2c39125ab3c04a8c4daaf1bfcc742
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-08-03 15:07:45 +1000

    First cut of 01-leap-year implementation

--- 01-leap-year/LeapYear.swift
+++ 01-leap-year/LeapYear.swift
@@ -1,3 +1,9 @@
 func isLeap(year: Int) -> Bool {
-    fatalError()
+    if (year % 400 == 0) {
+        return true
+    } else if (year % 100 == 0) {
+        return false
+    } else {
+        return year % 4 == 0
+    }
 }
--- 01-leap-year/LeapYearTests.swift
+++ 01-leap-year/LeapYearTests.swift
@@ -2,4 +2,19 @@ import XCTest
 
 class LeapYearTests: XCTestCase {
 
+    func test_evenly_divisible_by_4_true() {
+        XCTAssertTrue(isLeap(2004))
+    }
+
+    func test_evenly_divisible_by_100_false() {
+        XCTAssertFalse(isLeap(1300))
+    }
+
+    func test_evenly_divisible_by_400_true() {
+        XCTAssertTrue(isLeap(2000))
+    }
+
+    func test_evenly_divisible_by_4_false() {
+        XCTAssertFalse(isLeap(2003))
+    }
 }
