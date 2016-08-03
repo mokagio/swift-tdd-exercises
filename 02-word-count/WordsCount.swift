70dda9103d6f2139dd58562b8f9c639c9347d616
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-08-03 15:15:44 +1000

    First cut of 02-word-count implementation

--- 02-word-count/WordsCount.swift
+++ 02-word-count/WordsCount.swift
@@ -1,3 +1,26 @@
 func wordsCount(input: String) -> [String: Int] {
-    fatalError()
+    guard input.isEmpty == false else {
+        return [:]
+    }
+
+    let cleanInput = input
+        .stringByReplacingOccurrencesOfString(".", withString: "")
+        .stringByReplacingOccurrencesOfString(",", withString: "")
+        .stringByReplacingOccurrencesOfString(";", withString: "")
+        .stringByReplacingOccurrencesOfString(":", withString: "")
+        .stringByReplacingOccurrencesOfString("!", withString: "")
+        .stringByReplacingOccurrencesOfString("?", withString: "")
+
+    let components = cleanInput.componentsSeparatedByString(" ")
+    var counts = [String: Int]()
+    components.forEach { word in
+        guard let value = counts[word] else {
+            counts[word] = 1
+            return
+        }
+
+        counts[word] = value + 1
+    }
+
+    return counts
 }
--- 02-word-count/WordsCountTests.swift
+++ 02-word-count/WordsCountTests.swift
@@ -2,4 +2,32 @@ import XCTest
 
 class WordsCountTests: XCTestCase {
 
+    func test_empty_string_empty_dictionary() {
+        XCTAssertEqual(wordsCount(""), [:])
+    }
+
+    func test_one_word_returns_one_word_and_one() {
+        let string = "pizza"
+        XCTAssertEqual(wordsCount(string), [string: 1])
+    }
+
+    func test_multiple_words_are_accounted_for() {
+        let string = "one two one three"
+        let expected = ["one": 2, "two": 1, "three": 1]
+        XCTAssertEqual(wordsCount(string), expected)
+    }
+
+    // Triangualtion: add more test cases for the same assertion to verify that there is actually a
+    // rule implemented rather than hardcode checks.
+    func test_more_multiple_words_are_accounted_for() {
+        let string = "one two three four"
+        let expected = ["one": 1, "two": 1, "three": 1, "four": 1]
+        XCTAssertEqual(wordsCount(string), expected)
+    }
+
+    func test_doesnt_consider_punctuation_marks() {
+        let string = "one, one! two. three?"
+        let expected = ["one": 2, "two": 1, "three": 1]
+        XCTAssertEqual(wordsCount(string), expected)
+    }
 }
